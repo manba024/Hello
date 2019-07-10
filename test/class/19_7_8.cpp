22898a5dbe68db74242ed2565d8892564b8f2df8
dj <1724017340@qq.com>
2019-07-10 21:34:10 +0800

    1、vector迭代器失效 2、翻转链表

--- test/class/19_7_8.cpp
+++ test/class/19_7_8.cpp
@@ -21,38 +21,84 @@ public:
 	virtual bool OnSocketCloseEvent() = 0;
 };
 
-//class CTCPClientSocket
-//{
-//	
-//};
+class A {
+public:
+	A(int a) { cout << "A() " << a << endl; };
+	virtual ~A() { cout << "~B()" << endl; };
+};
+class B : public A
+{
+public:
+	B(int a) :A(a) { cout << "B()" << endl; };
+	virtual ~B() { cout << "~B()" << endl; };
+};
+
 namespace a {
 	int count = 0;
 }
 int count = 0;
 
-void doSomeWork() {
-	std::cout << "dosomework函数正在另一个线程上运行。" << std::endl;
-	int data = ::count++;
-	// Pause for a moment to provide a delay to make
-	// threads more apparent.
-	std::this_thread::sleep_for(std::chrono::seconds(3));
-	std::cout << "工作线程调用的函数已结束。" << std::endl;
-}
-
+struct Node {
+	int value;
+	Node* next;
+};
+//头
+Node* HanderInit(const short len);
+void printList(Node* hander);
+Node* FlipList(Node* hander, const short len);
 int main()
 {
-	std::vector<std::thread> threads;
-
-	for (int i = 0; i < 10; ++i) {
-
-		threads.push_back(std::thread(doSomeWork));
-		std::cout << "main（）线程在启动新线程后调用此函数" << std::endl;
-	}
-
-	for (auto& thread : threads) {
-		thread.join();
-	}
-
+	Node* hander = HanderInit(10);
+	printList(hander);
+	cout << "-------" << endl;
+	hander = FlipList(hander, 10);
+	printList(hander);
 	getchar();
 	return 0;
+}
+
+Node* HanderInit(const short len)
+{
+	Node* hander = nullptr;//(Node*)malloc(sizeof(Node));
+	Node* tmp = nullptr;
+	for (short i = 0; i < len; i++)
+	{
+		if (0 == i)
+		{
+			hander = (Node*)malloc(sizeof(Node));
+			hander->value = i;
+			tmp = hander;
+			continue;
+		}
+		tmp->next = (Node*)malloc(sizeof(Node));
+		tmp->next->value = i;
+		tmp = tmp->next;
+		tmp->next = nullptr;
+	}
+	return hander;
+}
+
+void printList(Node * hander)
+{
+	while (hander != nullptr)
+	{
+		cout << "Node: " << hander->value << endl;
+		hander = hander->next;
+	}
+}
+
+Node* FlipList(Node * hander, const short len)
+{
+	Node *pCurrent, *pPre, *pNext;
+	pPre = hander;
+	pCurrent = hander->next;
+	while (pCurrent)
+	{
+		pNext = pCurrent->next;
+		pCurrent->next = pPre;
+		pPre = pCurrent;
+		pCurrent = pNext;
+	}
+	hander->next = nullptr;
+	return pPre;
 }
